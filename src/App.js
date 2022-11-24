e143c0a3498198a4ea4bb1a17c9cc941838de50c
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-11-25 00:15:46 +0800

    feat: react baseline

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef } from 'react';
+import './App.css';
+import sketch from './sketch';
+import p5 from "p5";
+
+const App = () => {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvasDivElement = canvasRef.current;
+        // console.log(canvasRef);
+        new p5(sketch(canvasDivElement, 500, 500), canvasDivElement);
+    }, [])
+
+
+    return (
+        <div className="App">
+            <div ref={canvasRef} />
+        </div>
+    );
+}
+
+export default App;
